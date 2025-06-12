c795d65fe01f889cbee6a09b2aa29e5938c90894
Spanclihe Lasea Linri <spanclihelasea02@gmail.com>
2025-06-12 12:50:40 +0700

    Delete practiceaxa/repository directory

--- practiceaxa/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practiceaxa.repository;
-
-
-
-import com.example.practiceaxa.entity.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-}
--- practiceaxa/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practiceaxa.repository;
-
-
-import com.example.practiceaxa.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
-
